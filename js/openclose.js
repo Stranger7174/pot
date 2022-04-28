84443f76ce11b1a65c0ea57c29fe3586e976ff73
jch <rfv4317@naver.com>
2022-04-28 15:24:01 +0900

    Fistcomit

---
+++ js/openclose.js
@@ -0,0 +1,21 @@
+var effect = parent.document.styleSheets[1].cssRules[0];
+function opendocument() {
+  document.querySelector("#document").style.display = "block";
+}
+function opencontent() {
+  document.querySelector("#content").style.display = "block";
+}
+function openmemo() {
+  document.querySelector("#memo").style.display = "block";
+}
+
+function documentclose() {
+  effect.appendRull.animation.direction("reverse");
+  parent.document.getElementById("document").style.display = "none";
+}
+function contentclose() {
+  parent.document.getElementById("content").style.display = "none";
+}
+function memoclose() {
+  parent.document.getElementById("memo").style.display = "none";
+}
